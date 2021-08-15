a66e477bc829294747154e0b865da98f8f41ee4e
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-15 16:15:55 -0300

    feat: component navigation guard - beforeRouteEnter

--- src/views/contatos/ContatoEditar.vue
+++ src/views/contatos/ContatoEditar.vue
@@ -13,8 +13,21 @@
 <script>
 export default {
     props: ['id'],
-    created() {
-        console.log('Param:', this.$route.params)
+    data() {
+        return {
+            data: 'VueJS'
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log('beforeRouteEnter - Navigation Guard | Component |', 'to:', to.path, '-> from:', from.path)
+        // next()
+        // console.log('Data:', this.data)
+        if (to.query.autenticado === 'true') {
+            return next((vm) => {
+                console.log('Data:', vm.data)
+            })
+        }
+        next('/contatos')
     }
 }
 </script>
