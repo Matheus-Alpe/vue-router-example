7cb6481f1558e87525a176c755e382d223dafea5
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-15 11:59:17 -0300

    feat: route query

--- src/components/contatos/ContatosLista.vue
+++ src/components/contatos/ContatosLista.vue
@@ -2,10 +2,22 @@
     <div>
         <h3 class="font-weight-light">Contatos</h3>
 
-        <ul class="list-group" v-if="contatos.length > 0">
+        <div class="form-group">
+            <input 
+                type="search"
+                class="form-control"
+                placeholder="Buscar contatos"
+                @keyup.enter="buscar"
+                :value="$route.query.busca"
+            />
+        </div>
+
+        <hr>
+
+        <ul class="list-group" v-if="contatosFiltrados.length > 0">
             <ContatoListaItem 
                 class="list-group-item"
-                v-for="contato in contatos"
+                v-for="contato in contatosFiltrados"
                 :key="contato.id"
                 :contato="contato"
             />
@@ -36,6 +48,15 @@ export default {
         }
     },
 
+    computed: {
+        contatosFiltrados() {
+            const busca = this.$route.query.busca
+            return !busca
+                ? this.contatos
+                : this.contatos.filter(contato => contato.nome.toLowerCase().includes(busca.toLowerCase()))
+        }
+    },
+
      methods: {
 
         voltar() {
@@ -53,6 +74,15 @@ export default {
             // this.$router.go(-1) //indica quantas páginas seguir, exemplo: +1, -1
             // this.$router.forward()
             this.$router.back()
+        },
+
+        buscar(event) {
+            this.$router.push({
+                path: '/contatos',
+                query: {
+                    busca: event.target.value
+                }
+            })
         }
 
     }
